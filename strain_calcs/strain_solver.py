36193c2bca688da07d316385f6d3cc653ba7e98d
a37campb <aicamp4@hotmail.com>
2020-11-16 23:43:20 -0500

    major update to strain solver, duplicates produced

--- strain_calcs/strain_solver.py
+++ strain_calcs/strain_solver.py
@@ -3,34 +3,26 @@ import numpy as np
 from pathlib import Path as p
 from zipfile import ZipFile as z
 
-def calculator( ref_Xaxis, ref_Yaxis, ref_Zaxis, c_d = [11.90e11,5.34e11, 5.96e11], ai = [0.25,0.25,0.25]):
+def calculator( ref_Xaxis, ref_Yaxis, ref_Zaxis, stress, c_d , ai = [0.25,0.25,0.25]):
     # Setting up state
     row = 3
     col = 3
     size = (row,col)
     s_col = 6
-    stress = np.zeros(size)
     strain = np.zeros(size)
     stress_col = np.zeros((s_col,1))
 
     a = np.transpose( np.array([ref_Xaxis,ref_Yaxis,ref_Zaxis]) )
     ## Figure out the stress 3x3 system. 
-
-    c_unit_d = np.array([[c_d[0],c_d[1],c_d[1],0,0,0],[c_d[1],c_d[0],c_d[1],0,0,0],[c_d[1],c_d[1],c_d[0],0,0,0],[0,0,0,c_d[2],0,0],[0,0,0,0,c_d[2],0],[0,0,0,0,0,c_d[2]]])
+    r1 = [c_d[0],c_d[1],c_d[1],0,0,0]
+    r2 = [c_d[1],c_d[0],c_d[1],0,0,0]
+    r3 = [c_d[1],c_d[1],c_d[0],0,0,0]
+    r4 = [0,0,0,c_d[2],0,0]
+    r5 = [0,0,0,0,c_d[2],0]
+    r6 = [0,0,0,0,0,c_d[2]]
+    c_unit_d = np.array([r1,r2,r3,r4,r5,r6])
 
     c_unit = c_unit_d * 0.1 #conversion to Pa
-
-    # for i in range(row):
-    #     row_val = input('Enter the row' + str(i+1) + ' elements of stress in the reference axis specified separated by commas\n')
-    #     temp = []
-    #     while len(row_val) > 1:
-    #         delim = row_val.index(',')
-    #         temp += [float(row_val[:delim]),]
-    #         row_val = row_val[delim+1:]
-    #     temp += [float(row_val),]
-    #     stress[i] = temp
-
-    stress = [[1,0,0],[0,1,0],[0,0,1]]
 
     stress_unit = np.zeros(size)
     #assuming square.
@@ -80,7 +72,7 @@ c = [c11_d, c12_d, c44_d]
 # ref_Yaxis = [0,1/m.sqrt(2),-1/m.sqrt(2)]
 # ref_Zaxis = [0,1/m.sqrt(2),1/m.sqrt(2)]
 
-axis_vals = [[0, 0, 1], [0, m.sqrt(2), 1], [0, m.sqrt(2), 1] ]
+axis_vals = [[0, 0, 1], [0, 2*m.sqrt(2), 1], [0, m.sqrt(2), 1] ]
 folder = p.cwd() / "n471-proj-carrot" / "strain_calcs"
 # Modify folder to match your dir, currently on my dir. 
 name_cnt = 1
@@ -97,20 +89,43 @@ for g, x_val in enumerate(axis_vals):
         for j, z_val in enumerate(axis_vals):
             if j != g and j != h:
                 ref_Z= z_val
-                # do the calculations
-                sol = calculator(ref_X, ref_Y, ref_Z, c)
-                # store the values in text file in the format for nanohub
-                # open file
-                fname = "nano_hub_test_"+str(name_cnt)+".txt"
-                filename = folder / fname
-                f = open(filename, "w")
-                f.write("2\n"+compound)
-                f.write("Ga 0.0 0.0 0.0\n")
-                f.write("As " + str(sol[0]) + " " + str(sol[1]) + " " + str(sol[2]) + "\n")
-                f.close()
-                # zipper.write(filename)
-                # p.unlink(filename)
-                name_cnt += 1
+                #set up some system to define the stress values. 
+
+                # file w/ stress matrices
+                # while file.readline() -> read the first line
+                # do all the stuff
+                # have mutiple zip folders
+
+                stresser = folder / "stress.txt"
+                stress = []
+                with open(stresser, "r") as cs:
+                    for o, line in enumerate(cs.readlines()):
+                        row_val = line
+                        temp = []
+                        if "-" not in line:
+                            while row_val != "\n":
+                                delim = row_val.index(',')
+                                temp += [float(row_val[:delim]),]
+                                row_val = row_val[delim+1:]
+                            stress += [temp,]
+                        else:
+                            # do the calculations
+                            sol = calculator(ref_X, ref_Y, ref_Z, stress, c)
+                            # store the values in text file in the format for nanohub
+                            # open file
+                            fname = "nano_hub_test_"+str(name_cnt)+".txt"
+                            filename = folder / fname
+                            f = open(filename, "w")
+                            f.write("2\n"+compound)
+                            f.write("Ga 0.0 0.0 0.0\n")
+                            f.write("As " + str(sol[0]) + " " + str(sol[1]) + " " + str(sol[2]) + "\n")
+                            f.close()
+                            # zipper.write(filename)
+                            # p.unlink(filename)
+                            name_cnt += 1
+                            stress = []
+                            # make sure that the number of stress matrices equals the number of axis vals. 
+                    cs.close()
             else:
                 continue
 # zipper.close()
